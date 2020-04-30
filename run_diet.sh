ebf430814b4d9516dded5a353870955c6b0a58d1
K.Kobayashi <aziz.fizz@gmail.com>
2020-04-30 10:52:48 +0900

    fix

--- run_diet.sh
+++ run_diet.sh
@@ -4,9 +4,4 @@
 #$ -jc gpu-container_g1
 #$ -ac d=nvcr-torch-1712
 
-~/.pyenv/versions/anaconda3-5.2.0/envs/pytorch/bin/python train_k_holds.py -c config/DietNetworks/diet_recon_0.1.json -s
-~/.pyenv/versions/anaconda3-5.2.0/envs/pytorch/bin/python train_k_holds.py -c config/DietNetworks/diet_recon_0.05.json -s
-~/.pyenv/versions/anaconda3-5.2.0/envs/pytorch/bin/python train_k_holds.py -c config/DietNetworks/diet_recon_0.005.json -s
-~/.pyenv/versions/anaconda3-5.2.0/envs/pytorch/bin/python train_k_holds.py -c config/DietNetworks/diet_recon_0.5.json -s
-~/.pyenv/versions/anaconda3-5.2.0/envs/pytorch/bin/python train_k_holds.py -c config/DietNetworks/diet_recon_1.0.json -s
-~/.pyenv/versions/anaconda3-5.2.0/envs/pytorch/bin/python train_k_holds.py -c config/DietNetworks/diet_recon_5.0.json -s
+~/.pyenv/versions/anaconda3-5.2.0/envs/pytorch/bin/python train_k_holds.py -c config/DietNetworks/diet_baseline_batch.json -s
